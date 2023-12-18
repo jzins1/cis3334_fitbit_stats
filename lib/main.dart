136af6cd4dff759e56933d026ec12d37ba43cb25
zinsy23 <39246267+zinsy23@users.noreply.github.com>
2023-12-18 15:04:55 -0600

    Sprint 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'activity_model.dart';
 import 'sleep_model.dart';
 import 'temperature_model.dart';
 import 'rest_api.dart';
+import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -72,84 +73,133 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          const Text(
-            'Your Fitbit statistics (today):',
-            style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
+      body: SizedBox(
+        height: 500,
+        child: Column(
+          children: [
+            const Text(
+              'Your Fitbit statistics (today):',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          FutureBuilder(
-            future: futureActivitySummaries,
-            builder: (context, snapshot) {
-              if (snapshot == null || snapshot.connectionState == ConnectionState.none) {
-                return Container();
-              }
+            // EventCalendar(
+            //   calendarType: CalendarType.JALALI,
+            //   calendarLanguage: 'fa',
+            //   events: [
+            //     Event(
+            //       child: const Text('Laravel Event'),
+            //       dateTime: CalendarDateTime(
+            //         year: 1401,
+            //         month: 5,
+            //         day: 12,
+            //         calendarType: CalendarType.JALALI,
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            FutureBuilder(
+              future: futureActivitySummaries,
+              builder: (context, snapshot) {
+                print("Entering builder");
+                print("Snapshot is ${snapshot}");
+                if (snapshot == null || snapshot.connectionState == ConnectionState.waiting) {
+                  return Container();
+                }
 
-              Summary activitySummary = snapshot.data!;
-              int totalActivityMinutes = activitySummary.fairlyActiveMinutes +
-                  activitySummary.veryActiveMinutes;
+                print("Snapshot is ${snapshot}");
 
-              return Center(
-                child: Column(
-                  children: [
-                    Text('Number of steps: ${activitySummary.steps.toString()}'),
-                    Text(
-                      'Total minutes of activity: ${totalActivityMinutes} min '
-                          '(${activitySummary.fairlyActiveMinutes.toString()} min light and '
-                          '${activitySummary.veryActiveMinutes.toString()} min very active)',
+                if(snapshot.data! == null) {
+                  return Container();
+                } else {
+                  print("Snapshot is ${snapshot}");
+                  Summary activitySummary = snapshot.data!;
+                  print("Activity Minutes " + activitySummary.fairlyActiveMinutes.toString());
+                  int totalActivityMinutes = activitySummary
+                      .fairlyActiveMinutes +
+                      activitySummary.veryActiveMinutes;
+
+                  print("Snapshot is ${snapshot}");
+                  return Center(
+                    child: Column(
+                      children: [
+                        Text('Number of steps: ${activitySummary.steps
+                            .toString()}'),
+                        Text(
+                          'Total minutes of activity: ${totalActivityMinutes} min '
+                              '(${activitySummary.fairlyActiveMinutes
+                              .toString()} min light and '
+                              '${activitySummary.veryActiveMinutes
+                              .toString()} min very active)',
+                        ),
+                        Text('Resting heart rate: ${activitySummary
+                            .restingHeartRate} BPM'),
+                      ],
                     ),
-                    Text('Resting heart rate: ${activitySummary.restingHeartRate} BPM'),
-                  ],
-                ),
-              );
-            },
-          ),
-          FutureBuilder(
-            future: futureSleepSummaries,
-            builder: (context, snapshot) {
-              if (snapshot == null || snapshot.connectionState == ConnectionState.none) {
-                return Container();
-              }
+                  );
+                }
+              },
+            ),
+            FutureBuilder(
+              future: futureSleepSummaries,
+              builder: (context, snapshot) {
+                if (snapshot == null || snapshot.connectionState == ConnectionState.none) {
+                  return Container();
+                }
+                if (snapshot.data! == null) {
+                  return Container();
+                } else {
+                  SleepDataSummary sleepSummary = snapshot.data!;
 
-              SleepDataSummary sleepSummary = snapshot.data!;
+                  return Center(
+                    child: Column(
+                      children: [
+                        Text('Total time in bed: ${_convertMinToHrMin(
+                            sleepSummary.totalTimeInBed)}'),
+                        Text('Total time asleep: ${_convertMinToHrMin(
+                            sleepSummary.totalMinutesAsleep)}'),
+                        Text('Total time awake: ${_convertMinToHrMin(
+                            sleepSummary.stages.wake)}'),
+                        Text('Total time in light sleep: ${_convertMinToHrMin(
+                            sleepSummary.stages.light)}'),
+                        Text('Total time in deep sleep: ${_convertMinToHrMin(
+                            sleepSummary.stages.deep)}'),
+                        Text('Total time in REM sleep: ${_convertMinToHrMin(
+                            sleepSummary.stages.rem)}'),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+            FutureBuilder(
+              future: futureTemperatures,
+              builder: (context, snapshot) {
+                if (snapshot == null || snapshot.connectionState == ConnectionState.none) {
+                  return Container();
+                }
 
-              return Center(
-                child: Column(
-                  children: [
-                    Text('Total time in bed: ${_convertMinToHrMin(sleepSummary.totalTimeInBed)}'),
-                    Text('Total time asleep: ${_convertMinToHrMin(sleepSummary.totalMinutesAsleep)}'),
-                    Text('Total time awake: ${_convertMinToHrMin(sleepSummary.stages.wake)}'),
-                    Text('Total time in light sleep: ${_convertMinToHrMin(sleepSummary.stages.light)}'),
-                    Text('Total time in deep sleep: ${_convertMinToHrMin(sleepSummary.stages.deep)}'),
-                    Text('Total time in REM sleep: ${_convertMinToHrMin(sleepSummary.stages.rem)}'),
-                  ],
-                ),
-              );
-            },
-          ),
-          FutureBuilder(
-            future: futureTemperatures,
-            builder: (context, snapshot) {
-              if (snapshot == null || snapshot.connectionState == ConnectionState.none) {
-                return Container();
-              }
+                if(snapshot.data! == null) {
+                  return Container();
+                } else {
+                  List<TempSkin> tempSkin = snapshot.data!;
 
-              List<TempSkin> tempSkin = snapshot.data!;
-
-              return Center(
-                child: Column(
-                  children: [
-                    Text('Relative skin temperature (from baseline in degrees F): '
-                        '${tempSkin[0].value.nightlyRelative.toString()}'),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
+                  return Center(
+                    child: Column(
+                      children: [
+                        Text(
+                            'Relative skin temperature (from baseline in degrees F): '
+                                '${tempSkin[0].value.nightlyRelative
+                                .toString()}'),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
